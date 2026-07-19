36926476d5c21358a0bff634bce80cb3bee623de
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2026-07-19 22:41:16 +0600

    feat: implement Portkey integration for Gemini and OpenAI and swap primary and fallback Groq slugs

--- Advanced_Production_Rag/app/llm/gemini/config.py
+++ Advanced_Production_Rag/app/llm/gemini/config.py
@@ -1,20 +1,16 @@
-# import Portkey from 'portkey-ai';
+from portkey_ai import Portkey
 
-# const portkey = new Portkey({
-#   apiKey: "jeWm9ijVzpdKXt+iMgEI4Oh7A+Ou"
-# });
+portkey = Portkey(
+  api_key = "jeWm9ijVzpdKXt+iMgEI4Oh7A+Ou"
+)
 
-# async function main() {
-#   const response = await portkey.chat.completions.create({
-#     messages: [
-#       { role: "system", content: "You are a helpful assistant" },
-#       { role: "user", content: "What is Portkey" }
-#     ],
-#     model: "@gemini/gemini-3.5-flash",
-#     MAX_TOKENS: 512
-#   });
+response = portkey.chat.completions.create(
+    model = "@gemini/gemini-3.5-flash",
+    messages = [
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": "What is Portkey"}
+    ],
+    MAX_TOKENS = 512
+)
 
-#   console.log(response.choices[0].message.content);
-# }
-
-# main();
+print(response.choices[0].message.content)
