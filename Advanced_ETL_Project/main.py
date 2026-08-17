539f596c43c86655649ffd0daf2796231c7035a0
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2026-08-17 13:43:23 +0600

    feat: implement data validation component and project utility structure

--- Advanced_ETL_Project/main.py
+++ Advanced_ETL_Project/main.py
@@ -1,13 +1,16 @@
-from networksecurity.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.logging.logger import logging
 from networksecurity.exception.network_security_exception import NetworkSecurityException
+from networksecurity.components.data_validation import DataValidation
+
 import sys 
 
 def main():
     print("Hello from advanced-etl-project!")
 
     try:
+        # Data Ingestion
         trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
@@ -15,6 +18,14 @@ def main():
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+
+        # Data Validation
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
     except Exception as e:
            raise NetworkSecurityException(e,sys)
 
